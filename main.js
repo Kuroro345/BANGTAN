479cb9d654e2610a7b0761cd00fc458315a9cb95
Kuroro345 <106121341+Kuroro345@users.noreply.github.com>
2022-05-25 10:10:43 -0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".container").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-  });
-
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.containerGrid', { delay: 350 });
-ScrollReveal().reveal('.footer', { delay: 350 });
